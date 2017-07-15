8dad78b554582eef90ad908a049b1be48a0ca00c
eastflag <eastflag@gmail.com>
2017-07-15 15:46:49 +0900

    설명 추가

--- src/app/jquery/jquery.component.ts
+++ src/app/jquery/jquery.component.ts
@@ -1,9 +1,28 @@
-import {Component} from "@angular/core";
+import {AfterViewInit, Component, OnInit} from "@angular/core";
+
+declare var $: any;
+declare var todo_list: any;
+declare var refresh: any;
 
 @Component({
   templateUrl: 'jquery.component.html',
   styleUrls: ['jquery.component.scss']
 })
-export class JqueryComponent {
+export class JqueryComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    // jquery
+    this.getTodoList();
+  }
+
+  getTodoList() {
+    $.ajax({
+      url: 'http://www.javabrain.kr:8080/api/todo',
+      method: 'get',
+      success: function(data) {
+        todo_list = data;
+        refresh();
+      }
+    })
+  }
 }
